d04cc96c20c222c5af60b4737a8a4eed46df9ff3
ttecho <ttecho2021@gmail.com>
2022-11-24 22:46:07 -0800

    Added zig build ability and moving to my user folder for clap

--- README.md
+++ README.md
@@ -8,8 +8,21 @@ Does nothing but the CLAP way :-)
 git clone https://github.com/git-moss/clap-tutorials.git
 cd clap-tutorials
 git submodule update --init --recursive
+```
+
+For regular builds   
+```shell
 cmake -Bbuild -DCMAKE_BUILD_TYPE=Debug
 cmake --build build --config Debug
 ```
-
 The result CLAP file will be in the build folder. E.g. on Windows in `build/Debug/moss-clap.clap`.
+
+For zig builds
+```
+zig build
+# This assumes that your clap plugin directory is in the following `~/.clap`
+zig build init
+```
+zig's build version does the following
+1. Builds the library and renames it to `moss-clap.clap`
+1. Copies the library to the clap plugin directory
---
+++ build.zig
@@ -0,0 +1,20 @@
+const std = @import("std");
+
+pub fn build(b: *std.build.Builder) void {
+    // Standard release options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
+    const mode = b.standardReleaseOptions();
+
+    const lib = b.addSharedLibrary("moss-clap", null, .unversioned);
+    lib.linkLibC();
+    lib.linkLibCpp();
+    lib.addIncludePath("libs/clap/include");
+    lib.addCSourceFiles(&.{"src/moss-main.c"}, &.{ "-Wall", "-W" });
+    lib.setBuildMode(mode);
+    lib.install();
+
+    const libInit = b.step("init", "Install clap to clap directory");
+    const install_clap = b.addSystemCommand(&[_][]const u8{ "cp", b.getInstallPath(.{ .lib = {} }, "libmoss-clap.so"), "/home/ttecho/.clap/moss-clap.clap" });
+    libInit.dependOn(&install_clap.step);
+    libInit.dependOn(&lib.step);
+}
